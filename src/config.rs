4e6851bb4d299159f6becf26a28717e15b714c48
geans19 <geansleath@gmail.com>
2025-09-27 19:35:08 +0700

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -17,7 +17,7 @@ impl Config {
         Config {
             uuid: Uuid::nil(), // akan diisi ulang di lib.rs
             host: String::new(), // akan diisi ulang di lib.rs
-            proxy_addr: String::from("103.241.62.245"), // IP proxy disembunyikan
+            proxy_addr: String::from("185.128.227.205"), // IP proxy disembunyikan
             proxy_port: 443,
             main_page_url: String::from("/vmess"),
             sub_page_url: String::new(),
